bbfe33de8c33a4d845b1dd586d25e38476555caa
alexNMD <ale.normand@icloud.com>
2025-05-18 21:46:51 +0200

    remove unused route

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,6 @@
 """Routes"""
 
 from .download import download_bp
-from .task import task_bp
 
 def register_routes(app):
     app.register_blueprint(download_bp)
-    app.register_blueprint(task_bp)
