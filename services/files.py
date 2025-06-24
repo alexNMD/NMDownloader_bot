386075a7348fc413df1fbec5ae0dd5012f06e264
AlexNMD <alexandre.normand@icloud.com>
2025-06-24 22:53:27 +0200

    Move methods to handle archived files into a service

---
+++ services/files.py
@@ -0,0 +1,50 @@
+import logging
+import os
+import shutil
+import tarfile
+import zipfile
+
+logger = logging.getLogger("celery")
+
+class FilesArchivedService:
+    def __init__(self, path: str):
+        self.path = path
+        self.archive_extensions = {
+        ".zip": self.list_zip_contents,
+        ".tar": self.list_tar_contents,
+        ".tar.gz": self.list_tar_contents,
+        ".tgz": self.list_tar_contents,
+        ".tar.bz2": self.list_tar_contents,
+        ".tbz": self.list_tar_contents,
+        ".tar.xz": self.list_tar_contents,
+        ".txz": self.list_tar_contents,
+        }
+
+
+    def list_zip_contents(self):
+        with zipfile.ZipFile(self.path, "r") as archive:
+            return archive.namelist()
+
+    def list_tar_contents(self):
+        with tarfile.open(self.path, "r:*") as archive:
+            return archive.getnames()
+
+    def handle_archive(self) -> list | None:
+        _extension = os.path.splitext(self.path)[1]
+        _parent_directory = os.path.dirname(self.path)
+
+        if not (_list_files_method := self.archive_extensions.get(_extension)):
+            return None
+
+        logger.info("Unpacking Archive")
+        extracted_files = _list_files_method()
+        shutil.unpack_archive(self.path, _parent_directory)
+        logger.info("Archive Unpacked")
+        os.remove(self.path)
+        logger.info(f"{self.path} deleted")
+        return extracted_files
+
+    def is_compressed(self) -> bool:
+        _file_extension = os.path.splitext(self.path)[1]
+
+        return _file_extension in self.archive_extensions.keys()
